3ae08e6428fbaa0f2d1a1d21ea102adf1fc48448
sadikgell <sadikgell@gmail.com>
2024-02-13 22:12:17 +0300

    script edited

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,18 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float playerHp;
+    [SerializeField] private float playerArmor;
+    [SerializeField] private float playerSpeed;
+    [SerializeField] private float playerAttack;
+    [SerializeField] private Rigidbody2D playerRb;
+    private Vector2 moveDir;
+
+    private void FixedUpdate()
     {
-        
+        InputManagement();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        playerRb.velocity = new Vector2(moveDir.x * playerSpeed, moveDir.y * playerSpeed);
+    }
+
+    private void InputManagement()
+    {
+        float moveX = Input.GetAxisRaw("Horizontal");
+        float moveY = Input.GetAxisRaw("Vertical");
+
+        moveDir = new Vector2(moveX, moveY).normalized;
+
     }
 }
