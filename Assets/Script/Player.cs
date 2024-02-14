a7b446205eac0d7dccc69649e0c7863c960ff420
sadikgell <sadikgell@gmail.com>
2024-02-14 22:55:59 +0300

    player attack edited

--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
@@ -10,8 +11,18 @@ public class Player : MonoBehaviour
     [SerializeField] private float playerArmor;
     [SerializeField] private float playerSpeed;
     [SerializeField] private float playerAttack;
+    [SerializeField] private float playerAttackSpeed; //çalýþma prensibi saniyede kaç kez saldýracaðýný gösteriyor
     [SerializeField] private Rigidbody2D playerRb;
+    private Transform playerTransform;
     private Vector2 moveDir;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody2D>();
+        playerTransform = GetComponent<Transform>();
+
+        FunctionRepeat();
+    }
 
     private void FixedUpdate()
     {
@@ -26,6 +37,12 @@ public class Player : MonoBehaviour
     private void Move()
     {
         playerRb.velocity = new Vector2(moveDir.x * playerSpeed, moveDir.y * playerSpeed);
+
+        if (moveDir.x < 0)
+            transform.rotation = Quaternion.Euler(0f, 0f, 0f);
+        else
+            transform.rotation = Quaternion.Euler(0f, 180f, 0f);
+
     }
 
     private void InputManagement()
@@ -35,5 +52,17 @@ public class Player : MonoBehaviour
 
         moveDir = new Vector2(moveX, moveY).normalized;
 
+    }
+
+    private void PlayerAttack()
+    {
+        Debug.Log("saldýrýyorum");
+    }
+
+    private void FunctionRepeat()
+    {
+        playerAttackSpeed = 60 / (playerAttackSpeed * 60);
+
+        InvokeRepeating("PlayerAttack", 0, playerAttackSpeed);
     }
 }
