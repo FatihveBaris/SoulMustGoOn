41739d7c34b726357613f4aa2d990f4aff90b7cf
FatihveBaris <117710225+FatihveBaris@users.noreply.github.com>
2024-02-17 20:15:16 +0300

    enemiesmanager updated

--- Assets/Script/EnemiesManager.cs
+++ Assets/Script/EnemiesManager.cs
@@ -4,49 +4,64 @@ using UnityEngine;
 
 public class EnemiesManager : MonoBehaviour
 {
-    [SerializeField] GameObject enemy;
+    [SerializeField] GameObject skeleton;
+    [SerializeField] GameObject goblin;
     [SerializeField] Vector2 spawnArea;
     [SerializeField] float spawnTimer;
+    [SerializeField] float spawnGoblin;
+    float timerGoblin;
     [SerializeField] GameObject player;
+    [SerializeField] float timerShorter;
     float timer;
+    
     bool hasSpawnTimerBeenReducedAt15 = false;
     bool hasSpawnTimerBeenReducedAt30 = false;
     bool hasSpawnTimerBeenReducedAt45 = false;
     bool hasSpawnTimerBeenReducedAt59 = false;
-    [SerializeField] float timerShorter;
-
     private void Update()
     {
+        int gameSeconds = (int)Time.timeSinceLevelLoad % 60;
+        int gameSeconds2 = (int)Time.timeSinceLevelLoad;
         timer -= Time.deltaTime;
+
         if(timer < 0f)
         {
             SpawnEnemy();
             timer = spawnTimer;
         }
-
+        timerGoblin -= Time.deltaTime;
+        if(timerGoblin < 0f && gameSeconds2 >= 60)
+        {
+            SpawnEnemy2();
+            timerGoblin = spawnGoblin;
+        }
         // Oyunun süresini al ve saniye cinsinden dönüştür
-        int gameSeconds = (int)Time.timeSinceLevelLoad % 60;
+        
 
         // Eğer oyunun süresi 15, 30, 45 veya 59. saniye ise
         if (gameSeconds == 15 && !hasSpawnTimerBeenReducedAt15)
         {
             // spawnTimer'ı 1.1'e böl
             spawnTimer /= timerShorter;
+            if(gameSeconds2 >= 60){spawnGoblin /= timerShorter;}
             hasSpawnTimerBeenReducedAt15 = true;
         }
         else if (gameSeconds == 30 && !hasSpawnTimerBeenReducedAt30)
         {
             spawnTimer /= timerShorter;
+            if(gameSeconds2 >= 60){spawnGoblin /= timerShorter;}
             hasSpawnTimerBeenReducedAt30 = true;
         }
         else if (gameSeconds == 45 && !hasSpawnTimerBeenReducedAt45)
         {
             spawnTimer /= timerShorter;
+            if(gameSeconds2 >= 60){spawnGoblin /= timerShorter;}
             hasSpawnTimerBeenReducedAt45 = true;
         }
         else if (gameSeconds == 59 && !hasSpawnTimerBeenReducedAt59)
         {
             spawnTimer /= timerShorter;
+            if(gameSeconds2 >= 60){spawnGoblin /= timerShorter;}
             hasSpawnTimerBeenReducedAt59 = true;
         }
         else if (gameSeconds != 15 && gameSeconds != 30 && gameSeconds != 45 && gameSeconds != 59)
@@ -64,8 +79,17 @@ public class EnemiesManager : MonoBehaviour
 
         position += player.transform.position;
 
-        GameObject newEnemy = Instantiate(enemy);
+        GameObject newEnemy = Instantiate(skeleton);
         newEnemy.transform.position = position;
+    }
+    private void SpawnEnemy2()
+    {
+        Vector3 position = GenerateRandomPosition();
+
+        position += player.transform.position;
+
+        GameObject newEnemy2 = Instantiate(goblin);
+        newEnemy2.transform.position = position;
     }
 
     private Vector3 GenerateRandomPosition()
